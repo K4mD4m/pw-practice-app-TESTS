f185af41ec36c9f58a92d01fa985f29c1395e9c0
K4mD4m <KamyszekDamian@outlook.com>
2025-12-17 12:27:26 +0100

    Date Picker Page Object

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test'
 import { NavigationPage } from '../page-objects/navigationPage'
 import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
+import { DatepickerPage } from '../page-objects/datepickerPage'
+import { on } from 'events'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
@@ -18,8 +20,12 @@ test('navigate to from page', async ({ page }) => {
 test('parameterized methods', async ({ page }) => {
     const navigateTo = new NavigationPage(page)
     const onFormLayoutsPage = new FormLayoutsPage(page)
+    const onDatepickerPage = new DatepickerPage(page)
 
     await navigateTo.formLayoutsPage()
     await onFormLayoutsPage.submitUsingTheGridFormWithCredentailsAndSelectOptions('test@test.com', 'Welcome1', 'Option 1')
     await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('John Smith', 'john.smith@test.com', false)
+    await navigateTo.datepickerPage()
+    await onDatepickerPage.selectCommonDatePickerDateFromToday(5)
+    await onDatepickerPage.selectDatePickerWithRangeFromToday(6, 15)
 })
